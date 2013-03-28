92b60e3e3831a17c6175290e947b2fea682961ad
David Mazza <github@dmzza.com>
2013-03-28 14:24:55 -0400

    Added my first button, label, action and outlet

--- CrystalBall/ViewController.h
+++ CrystalBall/ViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+- (IBAction)buttonPressed:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UILabel *predictionLabel;
 
 @end
--- CrystalBall/ViewController.m
+++ CrystalBall/ViewController.m
@@ -26,4 +26,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)buttonPressed:(UIButton *)sender {
+   self.predictionLabel.text = @"Definitely YES";
+}
 @end
